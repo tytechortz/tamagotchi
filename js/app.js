4574cbdaa3b9b277c3a4a4a1b24c5df93d30099f
James <tytechortz@gmail.com>
2018-09-24 14:35:51 -0600

    creates a class for character

---
+++ js/app.js
@@ -0,0 +1,13 @@
+class Bison {
+    constructor(hunger, sleepiness, boredom, age, name) {
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+        this.age = 0;
+        this.name = name;
+    }
+}
+
+
+
+
