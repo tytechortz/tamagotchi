88bffdfd62178b5a95f2323ea71bf0b52b5903e2
James <tytechortz@gmail.com>
2018-09-24 19:56:24 -0600

    adds hunger, sleepiess, boredom and age metrics

--- js/app.js
+++ js/app.js
@@ -10,6 +10,16 @@ class Bison {
 
 // instantiate class
 
-const tatanka = new Bison( 0, 0, 0, 0, 'tatanka');
+const tatanka = new Bison( 0, 0, 0, 0, 'Tatanka');
 
+const $body = $('body');
+const $section = $('<section id="stats">STATS');
 
+$body.append($section);
+console.log(tatanka);
+for( var key in tatanka) {
+    const $article = $("<article/>");
+    $section.append($article);
+    const $h3 = $("<h3/>").text(key + " = " + tatanka[key]);
+    $article.append($h3);
+}
